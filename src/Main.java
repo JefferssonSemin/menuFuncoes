20165fc76f33ca19667e121c382dd14fd0901c0b
JefferssonSemin <jeffesemin@hotmail.com>
2019-11-07 18:07:21 -0300

    adicionado nova logica

--- src/Main.java
+++ src/Main.java
@@ -4,64 +4,118 @@ public class Main {
 
     public static void main(String[] args) {
 
-        Integer numerosDigitados[] = new Integer[10];
+        Integer contador = 1;
+
+        Integer numerosDigitados[] = new Integer[11];
+        Integer posicaoMaiorNumero =0;
         Integer maiorNumero =0;
+        Integer somaImpares =0;
+
         Integer fatorial =0;
+        int aux = 0;
+        int i = 0;
 
 
-        for (Integer i = 0; i < 10; i++) {
+        for (i = 1; i < 11; i++) {
             String numero = JOptionPane.showInputDialog("Digite seu número");
             numerosDigitados[i] =  Integer.parseInt(numero);
-          }
+        }
 
+        while (contador > 0){
+            Integer opcao = lerOpcaoDoMenu();
 
-
-        Integer opcao = lerOpcaoDoMenu();
-
-        switch (opcao) {
+            switch (opcao) {
             case 1:
-                JOptionPane.showMessageDialog(null,"[Programa será encerado!]");
-                break;
-            case 2:
-                JOptionPane.showMessageDialog(null,"[Programa será encerado!]");
-                break;
-            case 3:
-                JOptionPane.showMessageDialog(null,"[Programa será encerado!]");
-                break;
-            case 4:
-                JOptionPane.showMessageDialog(null,"[Programa será encerado!]");
-                break;
-            case 5:
-                JOptionPane.showMessageDialog(null,"[Programa será encerado!]");
-                break;
-            case 6:
-                Integer numeros = numerosDigitados[0];
-                Integer num = 0;
-                for (Integer j = numeros; j != 0; j--){
-                    num += numeros * j;
-                }
-                System.out.println(num/numeros);
-
-                break;
-            case 7:
-                for(Integer j = 0; j <10; j++){
+                for(Integer j = 10; j> 0; j --){
                     System.out.println(numerosDigitados[j]);
                 }
                 break;
-            case 8:
-                for(Integer j = 0; j <10; j++){
-                    Integer numeroMaior = numerosDigitados[j];
-                    if (numeroMaior > maiorNumero)
-                        maiorNumero = numeroMaior;
+            case 2:
+                for(Integer j = 1; j< 11; j ++){
+                    Integer numero = numerosDigitados[j];
+                    if (numero ==1)continue;
+                        if (ehPrimo(numero))
+                            System.out.println(numero);
                 }
-                System.out.println(maiorNumero);
+                break;
+            case 3:
+                for(Integer j = 1; j< 11; j ++){
+                    Integer numero = numerosDigitados[j];
+                    if(j % 2 != 0)
+                        somaImpares = somaImpares + numero;
+                }
+                JOptionPane.showMessageDialog(null,"A soma das posições ímpares da coleção é "+ somaImpares);
+                somaImpares = 0;
+                break;
+            case 4:
+                for(i = 0; i<11; i++){
+                    for(int j = 0; j<10; j++){
+                        if(numerosDigitados[j]!= null) {
+                            if (numerosDigitados[j] > numerosDigitados[j + 1]) {
+                                aux = numerosDigitados[j];
+                                numerosDigitados[j] = numerosDigitados[j + 1];
+                                numerosDigitados[j + 1] = aux;
+                            }
+                        }
+                    }
+                }
+                break;
+            case 5:
+                for(i = 0; i<11; i++){
+                    for(int j = 0; j<10; j++){
+                        if(numerosDigitados[j]!= null) {
+                            if (numerosDigitados[j] < numerosDigitados[j + 1]) {
+                                aux = numerosDigitados[j];
+                                numerosDigitados[j] = numerosDigitados[j + 1];
+                                numerosDigitados[j + 1] = aux;
+                            }
+                        }
+                    }
+                }
+                break;
+            case 6:
+                Integer numeros = numerosDigitados[1];
+                Integer num = 0;
+                for (Integer j = numeros; j > 0 ; j--){
+                        num = numeros * j;
+                        numeros = num;
+                }
+                JOptionPane.showMessageDialog(null,"O fatorial do primeiro numero da coleção é "+ num/numerosDigitados[1]);
 
+                break;
+            case 7:
+                for(Integer j = 0; j <11; j++){
+                    if( numerosDigitados[j] != null) {
+                        System.out.println(numerosDigitados[j]);
+                    }
+                }
+                break;
+            case 8:
+                for(Integer j = 1; j <11; j++){
+                        Integer numeroMaior = numerosDigitados[j];
+                        if (numeroMaior > maiorNumero) {
+                            maiorNumero = numeroMaior;
+                            posicaoMaiorNumero = j;
+                        }
+                }
+                JOptionPane.showMessageDialog(null,"A posição do maior número é "+posicaoMaiorNumero);
+                posicaoMaiorNumero =0;
                 break;
             case 9:
                 JOptionPane.showMessageDialog(null,"Até mais :D");
+                contador--;
                 break;
+            }
         }
+    }
 
+    private static boolean ehPrimo(int numero) {
+        for (int j = 2; j < numero; j++) {
+
+            if (numero % j == 0)
+                return false;
+        }
+        return true;
     }
 
     private static Integer lerOpcaoDoMenu(){
