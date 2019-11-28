49b41e2f05ebb38dd93089666f409cc998a2b018
Jeffersson Semin <jefferssonsemin@MacBook-Pro-de-Jeffersson.local>
2019-11-27 23:33:12 -0300

    adicionado arquivo que estava separado

--- src/Trabalho3.java
+++ src/Trabalho3.java
@@ -18,42 +18,122 @@ public class Trabalho3 {
         switch (menu){
 
             case 1:
+                for (int i = 0; i < numeroColecao; i++)
+                {
+                    boolean ePrimo = true;
+                    int numero = colecao[i];
+                    for (int j = 2; j < numero; j++)
+                    {
+                        if (numero % i == 0)
+                        {
+                            ePrimo = false;
+                        }
+                    }
 
+                    if (ePrimo)
+                    {
+                        System.out.println(colecao[i] + " é primo posição " + i);
+                    }
+                }
                 break;
 
 
             case 2:
 
+                for (int i = 0; i < numeroColecao; i++)
+                {
+                    for (int j = i; j < numeroColecao; j++)
+                    {
+                        if (colecao[i] % 2 == 1)
+                        {
+                            if (colecao[j] % 2 == 0)
+                            {
+                                int valorImpar = colecao[i];
+                                colecao[i] = colecao[j];
+                                colecao[j] = valorImpar;
+                            }
+                        }
+                    }
+                }
+
+                for (int i = 0; i < numeroColecao; i++)
+                {
+                    System.out.println(colecao[i]);
+                }
+
                 break;
 
             case 3:
+                int fatorial = 1;
+                for (int n = 1; n <= colecao[0]; n++)
+                {
+                    fatorial *= n;
+                }
+                System.out.println(fatorial);
 
                 break;
 
             case 4:
-                for (int i = numeroColecao; i != 0; i--) {
+
+                for (int i = 0; i < numeroColecao / 2; i++)
+                {
+                    int tmp = colecao[i];
+                    colecao[i] = colecao[numeroColecao - i - 1];
+                    colecao[numeroColecao - i - 1] = tmp;
+                }
+
+                for (int i = 0; i < numeroColecao; i++)
+                {
                     System.out.println(colecao[i]);
                 }
                     break;
 
             case 5:
-                for (int i = 0; i< numeroColecao; i++){
-                    for (int j = 1; i< numeroColecao; i++){
-                        if (colecao[i] == colecao[j]){
-                            repetido = true;
+                for (int i = 0; i < numeroColecao; i++)
+                {
+                    for (int j = 0; j < numeroColecao; j++)
+                    {
+                        if (i != j)
+                        {
+                            if (colecao[i] == colecao[j])
+                            {
+
+                                System.out.println("Tem repetido");
+                                break;
+                            }else
+                                System.out.println("Não tem repetido");
+
                         }
                     }
                 }
-                System.out.println(repetido);
 
                 break;
 
             case 6:
-                if (true){
+                int numeroRepetiuMais = 0;
+                int quantidadeRepetiuMais = 0;
+                for (int i = 0; i < numeroColecao; i++)
+                {
+                    int repeticao = 0;
+                    for (int j = 0; j < numeroColecao; j++)
+                    {
+                        if (i != j)
+                        {
+                            if (colecao[i] == colecao[j])
+                            {
+                                repeticao++;
+                            }
+                        }
+                    }
 
-                }else{
-                    System.out.println(colecao[0]);
+                    if (repeticao > quantidadeRepetiuMais)
+                    {
+                        numeroRepetiuMais = colecao[i];
+                        quantidadeRepetiuMais = repeticao;
+                    }
                 }
+                System.out.println("O numero " + numeroRepetiuMais + " e repetiu " + quantidadeRepetiuMais);
+
                 break;
 
             case 7:
